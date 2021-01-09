979d270a66971e6eb230701be6f0f01f6f5cda41
Harneet Kaur <74712190+Harneett@users.noreply.github.com>
2021-01-09 16:16:59 +0530

    Update If_Else_2_Demo.py

--- If_Else_2_Demo.py
+++ If_Else_2_Demo.py
@@ -4,11 +4,11 @@ provinceCanada = ["Alberta","British Columbia","Maitoba","New Brunswick","Nova S
 if detail1 == "Canada":
     detail2 = input("Enter your province")
     if detail2 == "Alberta":
-        print("Your total charge for the order is " + str(value1) + " + 0.5% General Sales Tax(GST), with a total of " +str(value1*0.005))
+        print("Your total charge for the order is " + str(value1) + " + 0.5% General Sales Tax(GST), with a total of " +str(value1+(value1*0.005)))
     elif detail2 == "Ontario" or detail2 == "New Brunswick" or detail2 == "Nova Scotia":
-        print("Your total charge for the order is " + str(value1) + " + 0.13% Harmonised Sales Tax, with a total of " + str(value1)+str(value1*0.0013))
+        print("Your total charge for the order is " + str(value1) + " + 0.13% Harmonised Sales Tax, with a total of " + str(value1+(value1*0.0013)))
     else:
-        print("Your total charge for the order is " + str(value1) + " + 0.6% Provincial Sales Tax" + "+0.5% General Sales Tax(GST), with a total of " + str(value1)+str(value1*0.006)+str(value1*0.005))
+        print("Your total charge for the order is " + str(value1) + " + 0.6% Provincial Sales Tax" + "+0.5% General Sales Tax(GST), with a total of " + str(value1+(value1*0.006)+(value1*0.005)))
 else:
     print("No additional Tax")
     print("Your total charge for the order is " + str(value1))
